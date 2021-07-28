aebcd2cb0b4d85665a53de84158e7e1c525038c9
Cristian Oliveros <3308@holbertonschool.com>
2021-07-28 18:24:10 -0500

    conditional in lines 23 - 31 and 45 - 46 for INT_MIN

--- itoa.c
+++ itoa.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
+
 /**
-* _itoa - change int to character
-* @i: the number
-* @strings: string that lloca the numers changed
-* @base: the base
-* Return: a string with the numbers changed to characters
-*/
+ * _itoa - change int to character
+ * @i: the number
+ * @strings: string that lloca the numers changed
+ * @base: the base
+ * Return: a string with the numbers changed to characters
+ */
 char *_itoa(int i, char *strings, int base)
 {
 	char *str = strings;
 	int digit = 0;
 	int sign = 0;
+	int cpy = i;
 
 	if (i == 0)
 	{
@@ -19,8 +21,14 @@ char *_itoa(int i, char *strings, int base)
 		return (strings);
 	}
 	if (i < 0)
+	{
 		sign = 1,
-			i = -i;
+			i = i * -1;
+		if (i == INT_MIN)
+		{
+			i = INT_MAX;
+		}
+	}
 	while (i)
 	{
 		digit = i % base;
@@ -34,14 +42,16 @@ char *_itoa(int i, char *strings, int base)
 	if (sign == 1)
 		*str++ = '-';
 	*str = '\0';
+	if (cpy == INT_MIN)
+		strings[0] = '8';
 	_reverse(strings);
 	return (strings);
 }
 /**
-* _reverse - reverse the string
-* @s: the string to reverse
-* Return: void
-*/
+ * _reverse - reverse the string
+ * @s: the string to reverse
+ * Return: void
+ */
 void _reverse(char *s)
 {
 	int i, j, k, m;
